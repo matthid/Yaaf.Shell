e9ebc187f9a1da09792f57ed58cd2bbf04328585
Matthias Dittrich <matthi.d@googlemail.com>
2015-04-02 00:47:28 +0200

    lock 0.38.2

--- packages/Yaaf.AdvancedBuilding/content/build.sh
+++ packages/Yaaf.AdvancedBuilding/content/build.sh
@@ -33,7 +33,7 @@ function do_build {
     fi
 
     echo "restore paket packages"
-    $MONO .paket/paket.exe -v restore
+    $MONO .paket/paket.exe -v restore 0.38.2
     exit_code=$?
     if [ $exit_code -ne 0 ]; then
       exit $exit_code
