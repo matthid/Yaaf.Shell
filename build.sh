5015d7c1ae79fa4ae55997d80922d8dbc6b3dd63
Matthias Dittrich <matthi.d@googlemail.com>
2015-04-02 00:32:24 +0200

    paket update

--- build.sh
+++ build.sh
@@ -4,6 +4,17 @@ then
   ./bootstrap.sh
 fi
 
-build="packages/Yaaf.AdvancedBuilding/content/build.sh"
-chmod +x "$build"
-"$build" $@
+"packages/Yaaf.AdvancedBuilding/content/build.sh" $@
+
+#do_build $@
+
+#if [ ! -d "temp" ]; then
+#  mkdir temp
+#fi
+#tmp_build="temp/build.sh"
+#cp "$build" "$tmp_build"
+#chmod +x "$tmp_build"
+#"$tmp_build" $@
+#exit_code=$?
+#rm "$tmp_build"
+#exit $exit_code
